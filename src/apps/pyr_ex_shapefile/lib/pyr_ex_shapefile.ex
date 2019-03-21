d35d18b51ee6df1a30048b102e361037677e2a1f
M. Simon Borg <msimonborg@gmail.com>
2019-03-21 15:54:52 -0400

    PYRExShapefile.map_shapes/1

--- src/apps/pyr_ex_shapefile/lib/pyr_ex_shapefile.ex
+++ src/apps/pyr_ex_shapefile/lib/pyr_ex_shapefile.ex
@@ -2,6 +2,7 @@ defmodule PYRExShapefile do
   @moduledoc """
   Read shapefile data.
   """
+
   @shp_dir "#{File.cwd!}/shp"
 
   def shp(shapefile) do
@@ -14,18 +15,72 @@ defmodule PYRExShapefile do
     |> Exshape.Dbf.read()
   end
 
+  @doc """
+  Decodes data from a Zip archived shapefile.
+
+  ## Example
+
+      [{"cb_2017_us_cd115_5m", proj, shapes}] = PYRExShapefile.from_zip("cb_2017_us_cd115_5m")
+      shapes |> Enum.take(1)
+      #=> [{%Exshape.Shp.Header{...}, %Exshape.Dbf.Header{...}}]
+  """
   def from_zip(shapefile) do
-    Exshape.from_zip("#{@shp_dir}/#{shapefile}.zip")
+    [{_, _, shapes}] = Exshape.from_zip("#{@shp_dir}/#{shapefile}.zip")
+    shapes
   end
 
-  def exshape_to_geo(%Exshape.Shp.Polygon{points: [polygon]}) do
-    coordinates = Enum.map(polygon, fn points ->
-      Enum.map(points, fn %{x: lon, y: lat} ->
-        {lat, lon}
+  @doc """
+  Parses unzipped shapefile data into maps.
+  """
+  def map_shapes(shapes) do
+    [{_, dbf_header}] = Enum.take(shapes, 1)
+    headers =
+      dbf_header.columns
+      |> Stream.map(fn column -> column.name end)
+      |> Enum.with_index()
+
+    {maps, _} =
+      shapes
+      |> Stream.drop(1)
+      |> Enum.map_reduce(headers, fn {shp, dbf}, headers ->
+        result =
+          Enum.reduce(headers, %{geom: exshape_to_geo(shp)}, fn {attr, index}, map ->
+            key = attr |> String.downcase() |> String.to_atom()
+            Map.put(map, key, Enum.at(dbf, index))
+          end)
+        {result, headers}
+      end)
+
+    maps
+  end
+
+  @doc """
+  Transforms an `Exshape.Shp.Polygon.t()` to a `Geo.Polygon.t()`.
+
+  ## Examples
+
+      iex> PYRExShapefile.exshape_to_geo(%Exshape.Shp.Polygon{points: [[[
+      ...>   %Exshape.Shp.Point{x: -96.639704, y: 42.737071},
+      ...>   %Exshape.Shp.Point{x: -96.635886, y: 42.741002},
+      ...>   %Exshape.Shp.Point{x: -96.632314, y: 42.745641}
+      ...> ]]]})
+      %Geo.MultiPolygon{coordinates: [[[
+        {42.737071, -96.639704},
+        {42.741002, -96.635886},
+        {42.745641, -96.632314}
+      ]]], srid: 3857}
+  """
+  def exshape_to_geo(%Exshape.Shp.Polygon{points: polygons}) do
+    coordinates = Enum.map(polygons, fn polygon ->
+      Enum.map(polygon, fn points ->
+        Enum.map(points, fn %{x: lon, y: lat} ->
+          {lat, lon}
+        end)
       end)
     end)
 
-    %Geo.Polygon{coordinates: coordinates, srid: srid()}
+
+    %Geo.MultiPolygon{coordinates: coordinates, srid: srid()}
   end
 
   @doc """
--- src/apps/pyr_ex_shapefile/test/pyr_ex_shapefile_test.exs
+++ src/apps/pyr_ex_shapefile/test/pyr_ex_shapefile_test.exs
@@ -1,4 +1,53 @@
 defmodule PYRExShapefileTest do
   use ExUnit.Case
   doctest PYRExShapefile
+
+  @shapes [
+    {%Exshape.Shp.Header{},
+      %Exshape.Dbf.Header{
+        columns: [
+          %Exshape.Dbf.Column{name: "STATEFP"},
+          %Exshape.Dbf.Column{name: "CD115FP"},
+          %Exshape.Dbf.Column{name: "AFFGEOID"},
+          %Exshape.Dbf.Column{name: "GEOID"},
+          %Exshape.Dbf.Column{name: "LSAD"},
+          %Exshape.Dbf.Column{name: "CDSESSN"},
+          %Exshape.Dbf.Column{name: "ALAND"},
+          %Exshape.Dbf.Column{name: "AWATER"}
+        ]
+      }},
+    {
+      %Exshape.Shp.Polygon{
+        points: [[[
+          %Exshape.Shp.Point{x: -96.639704, y: 42.737071},
+          %Exshape.Shp.Point{x: -96.635886, y: 42.741001999999995}
+        ]]]},
+      ["19", "04", "5001500US1904", "1904", "C2", "115", 58937921470, 264842664]
+    }
+  ]
+
+  @mapped_shapes [
+    %{
+      geom: %Geo.MultiPolygon{
+        coordinates: [[[
+          {42.737071, -96.639704},
+          {42.741001999999995, -96.635886}
+        ]]],
+        srid: 3857
+      },
+      statefp: "19",
+      cd115fp: "04",
+      affgeoid: "5001500US1904",
+      geoid: "1904",
+      lsad: "C2",
+      cdsessn: "115",
+      aland: 58937921470,
+      awater: 264842664
+    }
+  ]
+
+  test "maps shapefile data" do
+    assert PYRExShapefile.map_shapes(@shapes) == @mapped_shapes
+  end
+
 end
