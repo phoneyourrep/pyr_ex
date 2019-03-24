f4be50f2489b2775d492d53932597767afebf777
M. Simon Borg <msimonborg@gmail.com>
2019-03-24 00:19:46 -0400

    test controller actions for geo lookup

--- src/apps/pyr_ex_web/test/pyr_ex_web/controllers/jurisdiction_controller_test.exs
+++ src/apps/pyr_ex_web/test/pyr_ex_web/controllers/jurisdiction_controller_test.exs
@@ -20,9 +20,19 @@ defmodule PYRExWeb.JurisdictionControllerTest do
   }
   @invalid_attrs %{fips: nil, geoid: nil, name: nil, statefp: nil, type: nil}
 
+  @shape_attrs %{
+    geoid: "some geoid",
+    geom: %Geo.Point{coordinates: {1.0, 2.0}, srid: PYRExShapefile.srid()}
+  }
+
   def fixture(:jurisdiction) do
     {:ok, jurisdiction} = Geographies.create_jurisdiction(@create_attrs)
     jurisdiction
+  end
+
+  def fixture(:shape) do
+    {:ok, shape} = Geographies.create_shape(@shape_attrs)
+    shape
   end
 
   setup %{conn: conn} do
@@ -32,6 +42,35 @@ defmodule PYRExWeb.JurisdictionControllerTest do
   describe "index" do
     test "lists all jurisdictions", %{conn: conn} do
       conn = get(conn, Routes.jurisdiction_path(conn, :index))
+      assert json_response(conn, 200)["data"] == []
+
+      fixture(:jurisdiction)
+      conn = get(conn, Routes.jurisdiction_path(conn, :index))
+      assert [%{
+        "id" => id,
+        "fips" => "some fips",
+        "geoid" => "some geoid",
+        "name" => "some name",
+        "statefp" => "some statefp",
+        "type" => "some type"
+      }] = json_response(conn, 200)["data"]
+    end
+
+    test "lists jurisdictions for lat and lon", %{conn: conn} do
+      fixture(:jurisdiction)
+      fixture(:shape)
+
+      conn = get(conn, Routes.jurisdiction_path(conn, :index, lat: "1.0", lon: "2.0"))
+      assert [%{
+        "id" => id,
+        "fips" => "some fips",
+        "geoid" => "some geoid",
+        "name" => "some name",
+        "statefp" => "some statefp",
+        "type" => "some type"
+      }] = json_response(conn, 200)["data"]
+
+      conn = get(conn, Routes.jurisdiction_path(conn, :index, lat: "2.0", lon: "2.0"))
       assert json_response(conn, 200)["data"] == []
     end
   end
