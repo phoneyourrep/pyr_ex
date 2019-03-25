b56cc725b9b18f05749409e225431128f54381cc
M. Simon Borg <msimonborg@gmail.com>
2019-03-25 18:07:41 -0400

    catch :yamerl throws for better error reporting when running seeds

--- src/apps/pyr_ex/priv/repo/seeds.exs
+++ src/apps/pyr_ex/priv/repo/seeds.exs
@@ -15,101 +15,124 @@ alias PYREx.Geographies.{Shape, Jurisdiction}
 alias PYREx.Repo
 
 ##### Jurisdictions and Shapes #####
-shapefiles = YamlElixir.read_from_file!("apps/pyr_ex/priv/repo/shapefiles.yml")
-congress = shapefiles["congress"]
-states = shapefiles["states"]
-sldl = shapefiles["state_legislative_districts_lower"]
-sldu = shapefiles["state_legislative_districts_upper"]
+try do
+  shapefiles = YamlElixir.read_from_file!("apps/pyr_ex/priv/repo/shapefiles.yml")
+  congress = shapefiles["congress"]
+  states = shapefiles["states"]
+  sldl = shapefiles["state_legislative_districts_lower"]
+  sldu = shapefiles["state_legislative_districts_upper"]
 
-congress_tasks =
-  Enum.map(congress["filenames"], fn filename ->
-    fn ->
-      filename
-      |> Shapefile.map_download(congress["base_url"], timeout: :infinity, recv_timeout: :infinity)
-      |> Enum.each(fn shape ->
-        Shape.changeset(%Shape{}, shape)
-        |> Repo.insert!()
+  congress_tasks =
+    Enum.map(congress["filenames"], fn filename ->
+      fn ->
+        filename
+        |> Shapefile.map_download(congress["base_url"],
+          timeout: :infinity,
+          recv_timeout: :infinity
+        )
+        |> Enum.each(fn shape ->
+          Shape.changeset(%Shape{}, shape)
+          |> Repo.insert!()
 
-        jurisdiction =
-          shape
-          |> Map.drop([:geom])
-          |> Map.put(:name, shape.namelsad)
-          |> Map.put(:type, "us_cd")
+          jurisdiction =
+            shape
+            |> Map.drop([:geom])
+            |> Map.put(:name, shape.namelsad)
+            |> Map.put(:type, "us_cd")
 
-        Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
-        |> Repo.insert!()
-      end)
-    end
+          Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
+          |> Repo.insert!()
+        end)
+      end
+    end)
+
+  states_tasks =
+    Enum.map(states["filenames"], fn filename ->
+      fn ->
+        filename
+        |> Shapefile.map_download(states["base_url"], timeout: :infinity, recv_timeout: :infinity)
+        |> Enum.map(fn shape ->
+          Shape.changeset(%Shape{}, shape)
+          |> Repo.insert!()
+
+          jurisdiction =
+            shape
+            |> Map.drop([:geom])
+            |> Map.put(:type, "us_state")
+
+          Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
+          |> Repo.insert!()
+        end)
+      end
+    end)
+
+  sldl_tasks =
+    Enum.map(sldl["filenames"], fn filename ->
+      fn ->
+        filename
+        |> Shapefile.map_download(sldl["base_url"], timeout: :infinity, recv_timeout: :infinity)
+        |> Enum.each(fn shape ->
+          Shape.changeset(%Shape{}, shape)
+          |> Repo.insert!()
+
+          jurisdiction =
+            shape
+            |> Map.drop([:geom])
+            |> Map.put(:name, shape.namelsad)
+            |> Map.put(:type, "us_sldl")
+
+          Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
+          |> Repo.insert!()
+        end)
+      end
+    end)
+
+  sldu_tasks =
+    Enum.map(sldu["filenames"], fn filename ->
+      fn ->
+        filename
+        |> Shapefile.map_download(sldu["base_url"], timeout: :infinity, recv_timeout: :infinity)
+        |> Enum.each(fn shape ->
+          Shape.changeset(%Shape{}, shape)
+          |> Repo.insert!()
+
+          jurisdiction =
+            shape
+            |> Map.drop([:geom])
+            |> Map.put(:name, shape.namelsad)
+            |> Map.put(:type, "us_sldu")
+
+          Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
+          |> Repo.insert!()
+        end)
+      end
+    end)
+
+  [congress_tasks, states_tasks, sldl_tasks, sldu_tasks]
+  |> List.flatten()
+  |> Enum.chunk_every(3)
+  |> Enum.each(fn chunk ->
+    Enum.map(chunk, fn task -> Task.async(task) end)
+    |> Enum.each(fn task -> Task.await(task, :infinity) end)
   end)
+catch
+  {:yamerl_exception,
+   [
+     {:yamerl_parsing_error, :error, message, :undefined, :undefined, :file_open_failure,
+      :undefined, [error: :enoent]}
+   ]} ->
+    raise RuntimeError, """
+    Reading shapefiles manifest failed with message:
 
-states_tasks =
-  Enum.map(states["filenames"], fn filename ->
-    fn ->
-      filename
-      |> Shapefile.map_download(states["base_url"], timeout: :infinity, recv_timeout: :infinity)
-      |> Enum.map(fn shape ->
-        Shape.changeset(%Shape{}, shape)
-        |> Repo.insert!()
+        ** #{message} **
 
-        jurisdiction =
-          shape
-          |> Map.drop([:geom])
-          |> Map.put(:type, "us_state")
+    Make sure your current working directory is `src/` when running the seed file.
 
-        Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
-        |> Repo.insert!()
-      end)
-    end
-  end)
+    From the root project directory:
 
-sldl_tasks =
-  Enum.map(sldl["filenames"], fn filename ->
-    fn ->
-      filename
-      |> Shapefile.map_download(sldl["base_url"], timeout: :infinity, recv_timeout: :infinity)
-      |> Enum.each(fn shape ->
-        Shape.changeset(%Shape{}, shape)
-        |> Repo.insert!()
+        cd src/
+        mix run apps/pyr_ex/priv/repo/seeds.exs
 
-        jurisdiction =
-          shape
-          |> Map.drop([:geom])
-          |> Map.put(:name, shape.namelsad)
-          |> Map.put(:type, "us_sldl")
-
-        Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
-        |> Repo.insert!()
-      end)
-    end
-  end)
-
-sldu_tasks =
-  Enum.map(sldu["filenames"], fn filename ->
-    fn ->
-      filename
-      |> Shapefile.map_download(sldu["base_url"], timeout: :infinity, recv_timeout: :infinity)
-      |> Enum.each(fn shape ->
-        Shape.changeset(%Shape{}, shape)
-        |> Repo.insert!()
-
-        jurisdiction =
-          shape
-          |> Map.drop([:geom])
-          |> Map.put(:name, shape.namelsad)
-          |> Map.put(:type, "us_sldu")
-
-        Jurisdiction.changeset(%Jurisdiction{}, jurisdiction)
-        |> Repo.insert!()
-      end)
-    end
-  end)
-
-[congress_tasks, states_tasks, sldl_tasks, sldu_tasks]
-|> List.flatten()
-|> Enum.chunk_every(3)
-|> Enum.map(fn chunk ->
-  Enum.map(chunk, fn task -> Task.async(task) end)
-  |> Enum.map(fn task -> Task.await(task, :infinity) end)
-end)
-
+    """
+end
 ##### End of Jurisdictions and Shapes #####
