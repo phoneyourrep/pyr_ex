6abe4eb2c2e0e258d0233de922b39eb82fb004cb
M. Simon Borg <msimonborg@gmail.com>
2019-04-03 07:48:14 -0400

    mix ecto task aliases

--- src/mix.exs
+++ src/mix.exs
@@ -6,6 +6,7 @@ defmodule PYREx.Umbrella.MixProject do
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
       preferred_cli_env: [
         "pyr_ex.build": :test
       ]
@@ -27,6 +28,19 @@ defmodule PYREx.Umbrella.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19.3"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run apps/pyr_ex/priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"]
     ]
   end
 end
